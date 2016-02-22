085763024ab090e116f4346af3801583b51da688
Ruzana123 <Ruzana18@bigmir.net>
2016-02-22 14:17:34 +0200

    testfio

--- l2/homework/fio.php
+++ l2/homework/fio.php
@@ -1,9 +1,10 @@
 <pre>
 <?php
 $array = $_POST;
+/*
 $comma_separated = implode(" ", $array);
 $m  = $comma_separated;
-$s = explode(" ", $m);
-echo $s[0] . ' ' . substr($s[1],0,1) . '. ' . substr($s[2],0,1) . '. ';
+$s = explode(" ", $m);*/
+echo $array[last_name] . ' ' . substr($array[imya],0,1) . '. ' . substr($array[surname],0,1) . '. ';
 ?>
 <pre>
