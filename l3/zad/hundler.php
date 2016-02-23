d14b7e9d1c7384f5205689ce231da06071b4d3b1
Ruzana123 <Ruzana18@bigmir.net>
2016-02-23 16:41:36 +0200

    testf

--- l3/zad/hundler.php
+++ l3/zad/hundler.php
@@ -2,31 +2,20 @@
 <?php
 include 'function.php';
 
-$handle2 = fopen("data.txt", "r+");
 $n=rz_myfile("data.txt");
-fclose($handle2);
 
-if ($n<=4) {
-	$user = array('name'=>$_POST['name'],'email'=>$_POST['email']);
-	$string=serialize($user);
-	$handle=fopen("data.txt", "w");
-	fwrite($handle, $string);
-	print_r($user);
-	fclose($handle);
+
+if(($_POST['name']!=null)||($_POST['email']!=null)){
+
+	if($n>4){
+		$people=rz_get_data();
 	}
-	elseif(($_POST['name']!=null)||($_POST['email']!=null)){
-		$handle = fopen("data.txt", "r+");
-		$b=fread($handle,filesize("data.txt")); 
-		$array=unserialize($b); 
-		print_r($array); 
-		$user = array('name'=>$_POST['name'],'email'=>$_POST['email']); 
-		array_push($array, $user); 
-		print_r($array); 
-		$string=serialize($array); 
-		fclose($handle); 
-		$handle1=fopen("data.txt", "w"); 
-		fwrite($handle1, $string); 
-		fclose($handle1);
+
+	else{
+		$people = array();
+	}
+		rz_add_user($people); 
+
 	}
 	else echo 'Enter data';
 
