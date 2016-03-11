9d56e38ee009b77cd8f066610fa84b1a99568dea
Ruzana123 <Ruzana18@bigmir.net>
2016-03-11 15:01:35 +0200

    blog

---
+++ blog/blog/form.php
@@ -0,0 +1,441 @@
+<!DOCTYPE html>
+<html lang="en">
+	<!--head-->
+	<head>
+		<meta charset="UTF-8">
+		<title>Shop - blog detail</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="css/bootstrap.min.css" rel="stylesheet" >
+		<link href='https://fonts.googleapis.com/css?family=Crimson+Text' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+		<link href="css/style.css" rel="stylesheet" >
+		<link rel="stylesheet" href="css/jquery-ui.css">
+		<link href="css/responsive.css" rel="stylesheet">
+		<link href="css/owl.carousel.css" rel="stylesheet">
+		<link rel="stylesheet" href="css/magnific-popup.css">
+		<link rel="stylesheet" type="text/css" href="css/menu_sideslide.css">
+	</head>
+	<!--end head-->
+	<!--body-->
+	<body>
+		<!--header-->
+		<div id="show-menu-block"></div>
+		<header class="header-absolute">
+			<!--line-header-->
+			<div class="head-block">
+				<div class="line-header static-header">
+					<div class="container">
+						<div class="row-menu">
+							<div class="menu-wrap">
+								<nav class="menu">
+									<div class="icon-list">
+										<a href="index.html" class="logo"><img src="images/main/logo.png" alt=""></a>
+										<a href="index.html" class="active">Home</a>
+										<a href="shop.html">Shop</a>
+										<a href="soft.html">Soft</a>
+										<a href="cart.html">Cart</a>
+										<a href="check-out.html">Check</a>
+										<a href="blog-main.html">Blog</a>
+										<a href="blog.html">Blog-detail</a>
+										<a href="portfolio.html">Portfolio</a>
+										<a href="#" class="shopping-cart-logo-mini"><i class="fa fa-shopping-cart"></i></a>
+										<a href="#" class="search-logo-mini"><i class="fa fa-search"></i></a>
+									</div>
+								</nav>
+								<button class="close-button" id="close-button">Close Menu</button>
+							</div>
+							<button class="menu-button" id="open-button"></button>
+							<div class="logo-img-big">
+								<a href="index.html" class="logo"><img src="images/shop/logo.png" alt=""></a>
+							</div>
+							<div class="logo-fixed">
+								<a href="index.html" class="logo"><img src="images/main/logo.png" alt=""></a>
+							</div>
+							<div class="menu-top">
+								<nav class="navbar navbar-default">
+									<div class="container-fluid">
+										<div class="navbar-header">
+										</div>
+										<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+											<ul class="nav navbar-nav">
+												<li class="home-mega-menu"><a href="index.html" class="active">Home</a>
+												<ul class="mega-menu columns-5">
+													<li class="column">
+														<ul>
+															<li class="column-title"><a href="#">Interactive elements</a></li>
+															<li><a href="#">Our team</a></li>
+															<li><a href="#">Testimonials list</a></li>
+															<li><a href="#">Post lists</a></li>
+															<li><a href="#">Products lists</a></li>
+															<li><a href="#">Message boxes</a></li>
+														</ul>
+													</li>
+													<li class="column">
+														<ul>
+															<li class="column-title"><a href="#">Common elements</a></li>
+															<li><a href="#">Pricing tables</a></li>
+															<li><a href="#">Tabs</a></li>
+															<li><a href="#">Accordions</a></li>
+															<li><a href="#">Buttons</a></li>
+															<li><a href="#">ALerts box</a></li>
+														</ul>
+													</li>
+													<li class="column">
+														<ul>
+															<li class="column-title"><a href="#">Icon/graphics</a></li>
+															<li><a href="#">Counters</a></li>
+															<li><a href="#">Progress bars</a></li>
+															<li><a href="#">Pie charts</a></li>
+															<li><a href="#">Icon boxes</a></li>
+															<li><a href="#">Icons</a></li>
+														</ul>
+													</li>
+													<li class="column">
+														<ul>
+															<li class="column-title"><a href="#">Typography</a></li>
+															<li><a href="#">columns</a></li>
+															<li><a href="#">Dropcaps & lighlighter</a></li>
+															<li><a href="#">Heading styles</a></li>
+															<li><a href="#">Blockquotes</a></li>
+															<li><a href="#">Bullets list</a></li>
+														</ul>
+													</li>
+													<li class="column">
+														<ul>
+															<li class="column-title"><a href="#">Other</a></li>
+															<li><a href="#">Social icons</a></li>
+															<li><a href="#">Modals</a></li>
+															<li><a href="#">Google map</a></li>
+															<li><a href="#">Dividers</a></li>
+														</ul>
+													</li>
+												</ul>
+											</li>
+											<li class="shop-mega-menu"><a href="shop.html">Shop</a>
+											<ul class="mega-menu shop-column-5">
+												<li class="column">
+													<ul>
+														<li class="column-title"><a href="#">Men’s fashion</a></li>
+														<li><a href="#">Dress</a></li>
+														<li><a href="#">Coat</a></li>
+														<li><a href="#">Shoes & bags</a></li>
+														<li><a href="#">Beauty</a></li>
+														<li><a href="#">Jeans</a></li>
+													</ul>
+												</li>
+												<li class="column">
+													<ul>
+														<li class="column-title"><a href="#">Women fashion</a></li>
+														<li><a href="#">Suit & blazer</a></li>
+														<li><a href="#">Shoes & bags</a></li>
+														<li><a href="#">Accessories</a></li>
+														<li><a href="#">Sunglasses</a></li>
+														<li><a href="#">Shirts</a></li>
+													</ul>
+												</li>
+												<li class="column">
+													<ul>
+														<li class="column-title"><a href="#">Accesssories</a></li>
+														<li><a href="#">Trousers &</a></li>
+														<li><a href="#">Chinos</a></li>
+														<li><a href="#">T-Shirts</a></li>
+														<li><a href="#">Underwear</a></li>
+														<li><a href="#">Socks</a></li>
+													</ul>
+												</li>
+												<li class="column">
+													<ul>
+														<li class="column-title"><a href="#">Other</a></li>
+														<li><a href="#">Trousers &</a></li>
+														<li><a href="#">Chinos</a></li>
+														<li><a href="#">T-Shirts</a></li>
+														<li><a href="#">Underwear</a></li>
+														<li><a href="#">Socks</a></li>
+													</ul>
+												</li>
+												<div class="column">
+													<h3>END OF SEASON</h3>
+													<h2>OFF 70%</h2>
+													<a href="#" class="shop-column-btn">Shop now</a>
+												</div>
+											</ul>
+										</li>
+										<li class="soft-submenu"><a href="soft.html">Soft</a>
+										<ul class="column-menu">
+											<li><a href="#">Our team</a>
+										</li>
+										<li><a href="#" class="submenu-link">Testimonials list</a>
+										<ul class="column-submenu">
+											<li><a href="#">Counters</a></li>
+											<li><a href="#">Progress bars</a></li>
+											<li><a href="#">Pie charts</a></li>
+											<li><a href="#">Icon boxes</a></li>
+											<li><a href="#">Icons</a></li>
+										</ul>
+									</li>
+									<li><a href="#">Post lists</a></li>
+									<li><a href="#">Products lists</a></li>
+									<li><a href="#">Message boxes</a></li>
+								</ul>
+							</li>
+							<li><a href="cart.html">Cart</a></li>
+							<li><a href="check-out.html">Check</a></li>
+							<li class="dropdown">
+								<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">More<span class="caret"></span></a>
+								<ul class="dropdown-menu">
+									<li><a href="blog-main.html">Blog</a></li>
+									<li><a href="blog.html">Blog-detail</a></li>
+									<li><a href="portfolio.html">Portfolio</a></li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</nav>
+		</div>
+		<div class="shop-i">
+			<ul>
+				<li class="shopping"><a href="#"><i class="fa fa-shopping-cart"></i></a>
+				<div class="shopping-menu">
+					<div class="shopping-sale">
+						<a href="#"><img src="images/main/mmm.png" alt=""></a>
+						<div class="product-information">
+							<h4><a href="#">Jacquard floral coat</a></h4>
+							<span>$79.00</span>
+							<span class="qty">QTY: 01</span>
+						</div>
+					</div>
+					<div class="shopping-sale">
+						<a href="#"><img src="images/main/mmm.png" alt=""></a>
+						<div class="product-information">
+							<h4><a href="#">Flat sole patent sandal</a></h4>
+							<span>$35.00</span>
+							<span class="qty">QTY: 01</span>
+						</div>
+					</div>
+					<div class="subtotal">
+						<span>Subtotal: </span>
+						<span class="subtotal-price">$159.00 </span>
+						<a href="#" class="checkout-btn">Process to checkout</a>
+						<a href="#" class="view-cart-btn">View shopping cart</a>
+					</div>
+				</div>
+			</li>
+			<li class="search"><a href="#"><i class="fa fa-search"></i></a>
+			<div class="search-menu">
+				<form>
+					<div class="input-group">
+						<input type="text" class="form-control" placeholder="Enter Your Keywords...">
+						<button type="submit" class="search-btn">Search</button>
+					</div>
+				</form>
+			</div>
+		</li>
+	</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</header>
+		<div class="breadcrumbs">
+			<div class="container">
+				<div class="col-md-12 col-sm-12 col-xs-12 table-cell">
+					<h1>Blog with side bar</h1>
+					<ul class="top-ul">
+						<li><a href="#">Home</a></li>
+						<li><i class="fa fa-angle-right"></i></li>
+						<li><a href="http://test1.ru/php/blog/form.php">Blog</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+
+
+		<?php
+
+	        $servername = "localhost";
+	        $username = "ruzana";
+	        $password = "yzrjctyctq";
+
+	        try {
+	            $conn = new PDO("mysql:host=$servername;dbname=blog", $username, $password);
+	            // set the PDO error mode to exception
+	            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	           // echo "Connected successfully"; 
+	            }
+
+	        catch(PDOException $e) {
+	            echo "Connection failed: " . $e->getMessage();
+	            }
+	         try { 
+		        $stmt = $conn->prepare("INSERT INTO `post` (`Images`, `Title`, `Author`, `Description`, `Paper`) VALUES (:Images, :Title, :Author, :Description, :Paper)"); 
+		        $stmt->bindParam(':Images', $Images); 
+		        $stmt->bindParam(':Title', $Title); 
+		        $stmt->bindParam(':Author', $Author); 
+		        $stmt->bindParam(':Description', $Description); 
+		        $stmt->bindParam(':Paper', $Paper); 
+		        // insert a row 
+		        $Images = $_POST['Images']; 
+		        $Title = $_POST['Title'];
+		        $Author = $_POST['Author'];
+		        $Description = $_POST['Description'];
+		        $Paper = $_POST['Paper'];
+		        $stmt->execute(); 
+		    } 
+		    catch(PDOException $e) { 
+		        echo "Error: " . $e->getMessage(); 
+		    }
+	    ?>
+
+		<!-- product-base -->
+		<div class="container">
+			<div class="row content-blog">
+				<div class="comments" id="form-comment">
+						<div class="by-element">
+							<h2>Adding data</h2>
+						</div>
+						<form role="form" action="form.php" method="post">
+							<div class="form-group">
+								<div class="middle-group form-3">
+									<label class="form-label star">Title</label>
+									<input type="text" name="Title" pattern="^[A-Za-zА-Яа-яЁё\s]+$" class="form-control" minlength="2" maxlength="20" required>
+								</div>
+								<div class="middle-group form-3">
+									<label class="form-label star">Images</label>
+									<input type="text" name="Images" class="form-control" minlength="2" maxlength="20" required>
+								</div>
+								<div class="middle-group form-3">
+									<label class="form-label star">Author</label>
+									<input type="text" name="Author" pattern="^[A-Za-zА-Яа-яЁё\s]+$" class="form-control" minlength="2" maxlength="20" required>
+								</div>
+								<div class="middle-group">
+									<label class="form-label star">Description</label>
+									<textarea class="form-control textarea" name="Description" rows="3" placeholder="CONTENT"></textarea>
+								</div>
+								<div class="middle-group">
+									<label class="form-label star">Paper</label>
+									<textarea class="form-control textarea" name="Paper" rows="10" placeholder="CONTENT"></textarea>
+								</div>
+								<button type="submit" class="send">Adding data</button>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<footer>
+			<div class="footer-main">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-4 col-sm-6 footer-block">
+							<div class="about-corp">
+								<h4 class="footer-h4 first-footer-title yelow-line">Creative Agency from London - UK</h4>
+								<p class="alignment1">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+								<a href="index.html" class="logo-f"><img src="images/main/footer-logo.png" alt=""></a>
+							</div>
+						</div>
+						<div class="col-md-2 col-sm-6 footer-block">
+							<h4 class="footer-h4 yelow-line">Links</h4>
+							<ul class="footer-menu">
+								<li><a href="#">Theme features</a></li>
+								<li><a href="#">Page builder</a></li>
+								<li><a href="#">Private policy</a></li>
+								<li><a href="#">Shop ruler</a></li>
+								<li><a href="#">Services</a></li>
+								<li><a href="#">Gallery layout</a></li>
+							</ul>
+						</div>
+						<div class="col-md-3 col-sm-6 footer-block">
+							<h4 class="footer-h4 yelow-line">Twitter widget</h4>
+							<div class="alignment">
+								<div class="footer-info-post">
+									<p>Looking for an awesome CREATIVE WordPress Theme? Esquise was updated and optimized to run even better. Find it here: <a href="#">http://t.co/0WWEMQEQ48</a></p>
+									<i class="fa fa-twitter"></i>
+									<span>
+										01 day ago
+									</span>
+								</div>
+								<div class="footer-info-post">
+									<p>It is a long established fact that a reader will be distracted by the readable. Find it here: <a href="#">http://t.co/0WWEMQEQ48</a></p>
+									<i class="fa fa-twitter"></i>
+									<span>
+										02 day ago
+									</span>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6 footer-block">
+							<h4 class="footer-h4 yelow-line">Instagram</h4>
+							<div class="col-md-12 img-tab-f">
+								<ul class="alignment4">
+									<li>
+										<a class="footer-img" href="#"><img src="images/main/bg5.png" alt=""></a>
+									</li>
+									<li>
+										<a class="footer-img" href="#"><img src="images/main/bg5.png" alt=""></a>
+									</li>
+									<li>
+										<a class="footer-img" href="#"><img src="images/main/bg5.png" alt=""></a>
+									</li>
+									<li>
+										<a class="footer-img" href="#"><img src="images/main/bg5.png" alt=""></a>
+									</li>
+									<li>
+										<a class="footer-img" href="#"><img src="images/main/bg5.png" alt=""></a>
+									</li>
+									<li>
+										<a  class="footer-img" href="#"><img src="images/main/bg5.png" alt=""></a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="copyright">
+				<div class="container">
+					<div class="row footer-main">
+						<div class="col-md-8 col-sm-6 col-xs-12 left-footer">
+							<span>© 2015 ZAP CREATIVE PSD TEMPLATE. Powered By WPELITE</span>
+						</div>
+						<div class="col-md-4 col-sm-6 col-xs-12 right-footer">
+							<ul>
+								<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+								<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+								<li><a href="#"><i class="fa fa-globe"></i></a></li>
+								<li><a href="#"><i class="fa fa-behance"></i></a></li>
+								<li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
+								<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!-- end footer -->
+		<script src="js/jquery-1.11.3.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		<script src="js/jquery.magnific-popup.js"></script>
+		<script src="js/jquery.vide.min.js"></script>
+		<script src="js/owl.carousel.js"></script>
+		<script src="js/jquery-ui.js"></script>
+		<script src="js/main.js"></script>
+		<script src="js/classie.js"></script>
+		<script>
+		$('#open-popup').magnificPopup({
+		    items: [
+
+		      {
+		        src: 'va.mp4',
+		        type: 'iframe' // this overrides default type
+		      },
+
+		    ],   
+
+		});
+		</script>
+	</body>
+</html>
