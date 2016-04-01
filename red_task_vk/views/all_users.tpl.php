fa5cba1e52741e7997a6d05a4f69402fa61f2e65
Ruzana123 <Ruzana18@bigmir.net>
2016-04-01 11:12:05 +0300

    test

--- red_task_vk/views/all_users.tpl.php
+++ red_task_vk/views/all_users.tpl.php
@@ -1,16 +1,19 @@
 <div class="container">
 	<div>
 		<table class="table table-condensed table-hover">
-			<tr class="success">
-				<td>ID</td>
-				<td>NICK</td>
-				<td>EMAIL</td>
-				<td>VK_NAME</td>
-				<td>VK_IMG</td>
-				<td>VK_LINK</td>
-			</tr>
+				<thead>
+					<tr class="active">
+						<td>ID</td>
+						<td>NICK</td>
+						<td>EMAIL</td>
+						<td>VK NAME</td>
+						<td>AVATAR</td>
+						<td>VK LINK</td>
+						<td>ADMIN</td>
+					</tr>
+				</thead>
 			<?php foreach (all_users() as $key => $user) { ?>
-					<tr class="info">
+					<tr>
 						<td> <?php echo $user['id'] ?></td>
 						<td> <?php echo $user['nick'] ?></td>
 						<td> <?php echo $user['email'] ?></td>
@@ -19,9 +22,15 @@
 							<td> <?php echo '<img src="' .  $user['vk_img'] . '"/>'?> </td><?php
 						} 
 						else{ ?>
-							<td> <?php echo '<img src="' . "ava.jpg" . '"/>'?> </td><?php
+							<td> <?php echo '<img src="' . "ava.gif" . '"/>'?> </td><?php
 						}?>
 						<td><a href="https://vk.com/<?php echo $user['vk_link'] ?>"><?php echo $user['vk_link'] ?></a></td>
+						<?php if ($user['admin']==true){ ?>
+							<td><span class="glyphicon glyphicon-flag" style="margin-right: 10px;" aria-hidden="true"></td><?php
+						} 
+						else { ?>
+							<td><span class="glyphicon glyphicon-remove" style="margin-right: 10px;" aria-hidden="true"></td><?php
+						}?>
 					</tr>
 				<?php
 			} ?>
