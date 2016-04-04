7a6660449fa2aa5159b7d54cedac30ad93aec661
Ruzana123 <Ruzana18@bigmir.net>
2016-04-04 13:03:33 +0300

    blanc

---
+++ blanc/header.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+	<!--head-->
+	<head>
+		<meta charset="UTF-8">
+		<title>Classico</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- bootstrap.min -->
+		<link href="css/bootstrap.min.css" rel="stylesheet" >
+		<!-- fonts -->
+		<link href='https://fonts.googleapis.com/css?family=Raleway:400,100,100italic,200,200italic,300,300italic,400italic,500,500italic,600,600italic,700,700italic' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+		<!-- style.css -->
+		<link href="css/style.css" rel="stylesheet" >
+		<!-- responsive.css -->
+		<link href="css/responsive.css" rel="stylesheet">
+	</head>
+	<!--end head-->
+	<!--body-->
+	<body>
+		<!--header-->
+		<header class="site-header">
+			<!-- container -->
+			<div class="container"> 
+				<!-- header-bar -->
+				<div class="col-md-12 header-bar">
+					<!-- header-bar-content -->
+					<div class="header-bar-content">
+						<!-- secondary-left -->
+						<div class="secondary-left">
+							<span>Hello, we are Classico Commerce from London, UK</span>
+						</div>
+						<!--end secondary-left -->
+						<!--secondary-right-navigation -->
+						<nav class="secondary-right-navigation">
+							<ul class="secondary-menu">
+								<li class="menu-item">
+									<a href="#">My Account</a>
+								</li>
+								<li class="menu-item">
+									<a href="#">F&Q</a>
+								</li>
+								<li class="menu-item">
+									<a href="#">Advertisment</a>
+								</li>
+							</ul>
+						</nav>
+						<!--end secondary-right-navigation -->
+					</div>
+					<!--end header-bar-content -->
+				</div>
+				<!--end header-bar -->
+
+	
+				<!-- header-content -->
+				<div class="row header-content">
+					<div class="col-md-12">
+						<div class="col-md-4">
+							<a href="#" class="site-title">Blanc</a>
+						</div>
+						<div class="col-md-5">
+							<form class="form-search" method="GET" class="product-search">
+								<input type="search" name="blanc-search" class="search-field" placeholder="Search for product">
+									<!-- select -->
+										<select name="categories" id="categories" class="jquery-ui-select">
+											<option selected="selected">All categories</option>
+											<option>Category 2</option>
+											<option>Category 3</option>
+									    </select>
+									<!-- end select -->
+									<button type="submit"><i class="fa fa-search"></i></button>
+							</form>
+						</div>
+						<div class="col-md-3 site-header-cart">
+							<a href="#"><i class="fa fa-shopping-cart"></i><span>Shopping Bag </span></a><span class="dark-price">$255,99</span>
+						</div>
+					</div>
+				</div>
+				<!-- end header-content -->
+			</div>
+			<!-- end container -->
+
+				<!-- primary-navigation -->
+				<div class="primary-navigation">
+					<!-- navbar-default -->
+					<nav class="navbar navbar-default">
+	  					<div class="container-fluid">
+						    <div class="navbar-header">
+						      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						        <span class="sr-only">Toggle navigation</span>
+						        <span class="icon-bar"></span>
+						        <span class="icon-bar"></span>
+						        <span class="icon-bar"></span>
+						      </button>
+						    </div>
+
+							<!-- navbar-collapse -->
+						    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						      	<ul class="nav navbar-nav">
+							        <li class="active"><a href="#">HOME</a></li>
+							        <li class="dropdown">
+						          	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">SHOP<span class="caret"></span></a>
+						          	<ul class="dropdown-menu"><!-- dropdown-menu -->
+							            <li><a href="#">Action</a></li>
+							            <li><a href="#">Another action</a></li>
+							            <li><a href="#">Something else here</a></li>
+							            <li role="separator" class="divider"></li>
+							            <li><a href="#">Separated link</a></li>
+							            <li role="separator" class="divider"></li>
+							            <li><a href="#">One more separated link</a></li>
+						          </ul>
+						        </li>
+
+						        <li class="dropdown">
+						          	<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">BLOG<span class="caret"></span></a>
+						          	<ul class="dropdown-menu"><!-- dropdown-menu -->
+							            <li><a href="#">Action</a></li>
+							            <li><a href="#">Another action</a></li>
+							            <li><a href="#">Something else here</a></li>
+							            <li role="separator" class="divider"></li>
+							            <li><a href="#">Separated link</a></li>
+							            <li role="separator" class="divider"></li>
+							            <li><a href="#">One more separated link</a></li>
+						          	</ul>
+						        </li>
+
+				        		<li class="dropdown">
+				          			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">PORTFOLIO<span class="caret"></span></a>
+							        <ul class="dropdown-menu"><!-- dropdown-menu -->
+							            <li><a href="#">Action</a></li>
+							            <li><a href="#">Another action</a></li>
+							            <li><a href="#">Something else here</a></li>
+							            <li role="separator" class="divider"></li>
+							            <li><a href="#">Separated link</a></li>
+							            <li role="separator" class="divider"></li>
+							            <li><a href="#">One more separated link</a></li>
+							        </ul>
+							    </li>
+
+				        		<li class="dropdown">
+				          			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">SHORTCODES<span class="caret"></span></a>
+				          			<ul class="dropdown-menu"><!-- dropdown-menu -->
+							            <li><a href="#">Action</a></li>
+							            <li><a href="#">Another action</a></li>
+							            <li><a href="#">Something else here</a></li>
+							            <li role="separator" class="divider"></li>
+							            <li><a href="#">Separated link</a></li>
+							            <li role="separator" class="divider"></li>
+							            <li><a href="#">One more separated link</a></li>
+				          			</ul>
+				        		</li>
+						       <li><a href="#">CHECKOUT</a></li>
+						       <li><a href="#">BUY THEME!</a></li>
+				      		</ul>	<!--end navbar-nav -->
+			    		</div><!-- end navbar-collapse -->
+			  		</div><!-- end container-fluid -->
+				</nav><!--end navbar-default -->
+			</div>
+			<!--end primary-navigation -->
+		</div>
+	</header><!--end header-->
